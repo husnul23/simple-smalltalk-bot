582da17caf5dc18790f1e87e819e677b7774dd0b
Rasyid Respati Wiriaatmaja <rasyidrespati.30@gmail.com>
2019-01-25 09:00:02 +0700

    remove promise

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const Telegram = require("node-telegram-bot-api"); // initiate Node.js module to interact with official Telegram Bot API
 const Reply = require("./instance/axiosinstance"); //declare a variable reply from axios
 
-
-//Use the bluebird library to make promises
-const Promise = require('bluebird');
-Promise.config({
-  cancellation: true
-});
-
-
 //declare telegram bot token, what i got from botfather
 const token = '789653031:AAEtFAiLU2xch5C72ozmcX8YjtXYlUUu-Ms'; 
 const mondayCoolBot = new Telegram(token, { polling: true });
@@ -17,6 +9,7 @@ const port = process.env.PORT || 8080;
 //declare package use in this file express
 const express = require('express');
 const app = express();
+
 
 mondayCoolBot.on("message", chat => {
   var chatId = chat.chat.id;
