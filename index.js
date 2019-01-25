a3f08ba897f2dc362c052ec012ed4a2f82864580
husnul23 <batalkawin.hf@gmail.com>
2019-01-25 08:00:23 +0700

    fix bug

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const Telegram = require("node-telegram-bot-api");
-const Reply = require("./instance/axiosinstance");
+const Telegram = require("node-telegram-bot-api"); // initiate Node.js module to interact with official Telegram Bot API
+const Reply = require("./instance/axiosinstance"); //declare a variable reply from axios
+
+
+//Use the bluebird library to make promises
 const Promise = require('bluebird');
 Promise.config({
   cancellation: true
 });
-const token = '789653031:AAEtFAiLU2xch5C72ozmcX8YjtXYlUUu-Ms';
+
+
+//declare telegram bot token, what i got from botfather
+const token = '789653031:AAEtFAiLU2xch5C72ozmcX8YjtXYlUUu-Ms'; 
 const mondayCoolBot = new Telegram(token, { polling: true });
 const port = process.env.PORT || 8080;
+
+//declare package use in this file express
 const express = require('express');
 const app = express();
 
@@ -14,6 +22,8 @@ mondayCoolBot.on("message", chat => {
   var chatId = chat.chat.id;
   var message = chat.text.toString();
 
+
+//create a telegram bot function through a variable reply  
   Reply.get_reply(message, (err, res) => {
     if (!err) {
       mondayCoolBot.sendMessage(chatId, res);
--- instance/axiosinstance.js
+++ instance/axiosinstance.js
@@ -1,22 +1,26 @@
+//Transform request and response data, and axios can automatic transform for json data
 const axios = require("axios");
 
+//to read variable in the name is .env
 require("dotenv").config();
 
-// const DIALOGFLOW_API_URL = process.env.c41c4b59f6404a3bbb791f80f436d3a8;
+//to declare dailog flow token, what i get from dialog flow website
 const DIALOGFLOW_CLIENT_TOKEN = '14e30966c6a44ab4a14beb27ffa21bdf';
 
+//authentification from dialog flow token
 const AuthStr = "Bearer ".concat(DIALOGFLOW_CLIENT_TOKEN);
 
+//to send a function to use in another file
+//get url dialog flow
 module.exports = {
   get_reply: (chat, callback) => {
     axios
       .get(
         `https://api.dialogflow.com/v1/query?v=20170712&query=${chat}&lang=en&sessionId=fbf04ad8-cf33-3c76-c48d-52a563a0a525&timezone=Asia/Jakarta`,
         { headers: { Authorization: AuthStr } }
-
-        
-
       )
+
+      //create a function to handle API created from dialogflow
       .then(response => callback(null, response.data.result.fulfillment.speech))
       .catch(error => callback(error, null));
   }
